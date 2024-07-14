31b03e26e057c8b93ea664a2b833bfe46647aa6d
kost90 <kostdonc90@gmail.com>
2024-07-14 22:40:56 +0100

    edited styles of modal window

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -17,7 +17,7 @@ function Button({ type, onClick, name, className = "", children}: IProps) {
       type={type}
       onClick={onClick}
       className={clsx(
-        `flex items-center justify-center p-2 md:py-2 md:px-5 border border-black rounded-xl w-40 bg-orange text-black hover:bg-navBar hover:border-orange hover:text-cream active:bg-cream active:text-black transition duration-75 cursor-pointer`,
+        `flex items-center justify-center p-2 md:py-2 md:px-5 border border-black rounded-xl w-40 bg-orange text-black hover:bg-cream hover:border-orange hover:text-black hover:font-bold active:bg-cream active:text-black transition duration-75 cursor-pointer`,
         className
       )}
       disabled={pending}
